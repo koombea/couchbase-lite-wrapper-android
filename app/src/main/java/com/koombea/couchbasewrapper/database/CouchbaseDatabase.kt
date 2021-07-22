f7afdf3e99c434d8a8c9269c30c26844595bde99
Gabriel Bustos <juan.bustos@koombea.com>
2021-07-22 18:02:42 -0500

    Added copyright comment in every wrapper source code file

--- app/src/main/java/com/koombea/couchbasewrapper/database/CouchbaseDatabase.kt
+++ app/src/main/java/com/koombea/couchbasewrapper/database/CouchbaseDatabase.kt
@@ -1,3 +1,18 @@
+//
+// Copyright (c) 2021 Koombea, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package com.koombea.couchbasewrapper.database
 
 import android.content.Context
@@ -10,6 +25,8 @@ import java.util.*
 /**
  * Couchbase Wrapper with basic CRUD
  * The intended usage is to save only one type of object in each database instance
+ * If another kind of object would be saved, then create another CouchbaseDatabase to operate
+ * with that object type
  */
 class CouchbaseDatabase (
     private val context: Context,
